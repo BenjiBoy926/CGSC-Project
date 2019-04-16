8ac528a07f6f25e9d0bfa3da3dc1c0fc96380993
Autofire <Autofire@users.noreply.github.com>
2019-04-15 22:02:38 -0700

    Worked on line rope line stuff.

---
+++ Assets/Scripts/GrappleHookController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+public class GrappleHookController : MonoBehaviour
+{
+	private Rigidbody2D hookRigidbody;
+
+	private void Awake() {
+		hookRigidbody = GetComponent<Rigidbody2D>();
+		Assert.IsNotNull(hookRigidbody);
+	}
+
+	// Start is called before the first frame update
+	void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
